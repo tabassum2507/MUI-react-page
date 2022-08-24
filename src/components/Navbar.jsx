20453ef6a9adecd2eff290aa8f95ae29e2626f60
TABASSUM KHANUM <55125208+tabassum2507@users.noreply.github.com>
2022-08-24 22:31:46 +0530

    search bar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Toolbar, styled, Typography } from "@mui/material";
+import { AppBar, Toolbar, styled, Typography,  InputBase, Box } from "@mui/material";
 import EmojiNatureIcon from '@mui/icons-material/EmojiNature';
 
 const StyledToolbar = styled(Toolbar)({
@@ -7,7 +7,18 @@ const StyledToolbar = styled(Toolbar)({
   justifyContent: "space-between",
 });
 
-const Search = styled("div")
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)({
+  display: "none",
+  align: "center",
+  gap: "10px",
+})
 
 const Navbar = () => {
   return (
@@ -17,6 +28,12 @@ const Navbar = () => {
          CHIM CHIM
         </Typography>
         <EmojiNatureIcon sx={{display: {xs:"block", sm: "none"}}} />
+        <Search>
+          <InputBase placeholder="search..." />
+          search
+        </Search>
+        <Icons>
+        </Icons>
     </StyledToolbar>
     
   </AppBar>
