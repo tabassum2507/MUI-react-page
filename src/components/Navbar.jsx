4b3c3feaac96bcd24fa45c5fc687a02a57fd5192
TABASSUM KHANUM <55125208+tabassum2507@users.noreply.github.com>
2022-08-24 23:41:26 +0530

    icons avatar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,76 @@
 import React from "react";
-import { AppBar, Toolbar, styled, Typography,  InputBase, Box } from "@mui/material";
-import EmojiNatureIcon from '@mui/icons-material/EmojiNature';
+import {
+  AppBar,
+  Toolbar,
+  styled,
+  Typography,
+  InputBase,
+  Box,
+  Badge,
+  Avatar
+  // MailIcon,
+  // Notifications
+} from "@mui/material";
+import EmojiNatureIcon from "@mui/icons-material/EmojiNature";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
-  justifyContent: "space-between",
+  justifyContent: "space-between"
 });
 
 const Search = styled("div")(({ theme }) => ({
   backgroundColor: "white",
   padding: "0 10px",
   borderRadius: theme.shape.borderRadius,
-  width: "40%",
+  width: "40%"
 }));
 
-const Icons = styled(Box)({
-  display: "none",
+const Icons = styled(Box)(({ theme }) => ({
+  display: "flex",
   align: "center",
-  gap: "10px",
-})
+  gap: "20px"
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  align: "center",
+  gap: "20px"
+}));
 
 const Navbar = () => {
   return (
     <AppBar position="stick">
-    <StyledToolbar >
-    <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
-         CHIM CHIM
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          CHIM CHIM
         </Typography>
-        <EmojiNatureIcon sx={{display: {xs:"block", sm: "none"}}} />
+        <EmojiNatureIcon sx={{ display: { xs: "block", sm: "none" } }} />
         <Search>
           <InputBase placeholder="search..." />
           search
         </Search>
         <Icons>
+          <Badge badgeContent={4} color="error">
+            {/* <MailIcon /> */}
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            {/* <Notifications />  */}
+          </Badge>
+          <Avatar
+            src="https://images.unsplash.com/photo-1621346945190-51bcf3d4b96d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            sx={{ width: 30, height: 30 }}
+          />
+          <UserBox variant="span">
+            <Avatar
+              src="https://images.unsplash.com/photo-1621346945190-51bcf3d4b96d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              sx={{ width: 30, height: 30 }}
+            />
+            <Typography variant="span">John</Typography>
+          </UserBox>
         </Icons>
-    </StyledToolbar>
-    
-  </AppBar>
-  )
+      </StyledToolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
