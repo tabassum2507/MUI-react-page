9b818ac2306e4d5d0f0e8b2497b960a4801a73cd
tabassum2507 <10tabassum2@gmail.com>
2022-08-24 19:58:22 +0530

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { AppBar, Toolbar, styled, Typography } from "@mui/material";
+import EmojiNatureIcon from '@mui/icons-material/EmojiNature';
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <AppBar position="stick">
+    <StyledToolbar >
+    <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+         CHIM CHIM
+        </Typography>
+        <EmojiNatureIcon sx={{display: {xs:"block", sm: "none"}}} />
+    </StyledToolbar>
+    
+  </AppBar>
   )
-}
+};
 
-export default Navbar
+export default Navbar;
