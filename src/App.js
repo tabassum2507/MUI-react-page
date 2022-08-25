3927775b4e6905b23d205ee1c49083ddbe170baf
TABASSUM KHANUM <55125208+tabassum2507@users.noreply.github.com>
2022-08-25 15:48:21 +0530

    postadd started

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Feed from "./components/Feed";
 import Navbar from "./components/Navbar";
 import Rightbar from "./components/Rightbar";
 import Sidebar from "./components/Sidebar";
+import Add from "./components/Add";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Feed />
           <Rightbar />
         </Stack>
+        <Add />
       </Box>
     </div>
   );
--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Box } from "@mui/material/Box";
+import { Tooltip, Fab } from "@mui/material";
+import { Add as AddIcon, FullscreenExit } from "@mui/icons-material";
+import Modal from "@mui/material/Modal";
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+
+const SytledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
+});
 
 const Add = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>Add</div>
-  )
-}
+    <>
+      <Tooltip
+        onClick={(e) => setOpen(true)}
+        title="Delete"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50% - 25px)", md: 30 }
+        }}
+      >
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Tooltip>
 
-export default Add
+      <SytledModal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
+          <Typography variant="h6">Post Photos</Typography>
+        </Box>
+      </SytledModal>
+    </>
+  );
+};
+
+export default Add;
