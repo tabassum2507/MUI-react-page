e69f3a6a655c3f8ea81b5db25873763001d37f63
TABASSUM KHANUM <55125208+tabassum2507@users.noreply.github.com>
2022-08-25 16:02:14 +0530

    AddPost completed

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,15 +1,37 @@
+import {
+  Avatar,
+  Button,
+  ButtonGroup,
+  Fab,
+  Modal,
+  Stack,
+  styled,
+  TextField,
+  Tooltip,
+  Typography
+} from "@mui/material";
 import React, { useState } from "react";
-import { Box } from "@mui/material/Box";
-import { Tooltip, Fab } from "@mui/material";
-import { Add as AddIcon, FullscreenExit } from "@mui/icons-material";
-import Modal from "@mui/material/Modal";
-import { styled } from "@mui/material/styles";
-import Typography from "@mui/material/Typography";
+import {
+  Add as AddIcon,
+  DateRange,
+  EmojiEmotions,
+  Image,
+  PersonAdd,
+  VideoCameraBack
+} from "@mui/icons-material";
+import { Box } from "@mui/system";
 
 const SytledModal = styled(Modal)({
   display: "flex",
   alignItems: "center",
   justifyContent: "center"
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px"
 });
 
 const Add = () => {
@@ -29,15 +51,56 @@ const Add = () => {
           <AddIcon />
         </Fab>
       </Tooltip>
-
       <SytledModal
         open={open}
         onClose={(e) => setOpen(false)}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
-          <Typography variant="h6">Post Photos</Typography>
+        <Box
+          width={400}
+          height={280}
+          bgcolor="white"
+          color={"text.primary"}
+          p={3}
+          borderRadius={5}
+        >
+          <Typography variant="h6" color="gray" textAlign="center">
+            Create post
+          </Typography>
+          <UserBox>
+            <Avatar
+              src="https://images.unsplash.com/photo-1635542529858-566ad6c4b4a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+              sx={{ width: 30, height: 30 }}
+            />
+            <Typography fontWeight={500} variant="span">
+              Tuni
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: "100%" }}
+            id="standard-multiline-static"
+            multiline
+            rows={3}
+            placeholder="What's on your mind?"
+            variant="standard"
+          />
+          <Stack direction="row" gap={1} mt={2} mb={3}>
+            <EmojiEmotions color="primary" />
+            <Image color="secondary" />
+            <VideoCameraBack color="success" />
+            <PersonAdd color="error" />
+          </Stack>
+          <ButtonGroup
+            fullWidth
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button>Post</Button>
+            <Button sx={{ width: "100px" }}>
+              <DateRange />
+            </Button>
+          </ButtonGroup>
         </Box>
       </SytledModal>
     </>
