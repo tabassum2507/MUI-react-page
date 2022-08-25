8f76b2dc462fdb7ef80b11a773727f22eb577c2e
TABASSUM KHANUM <55125208+tabassum2507@users.noreply.github.com>
2022-08-25 16:27:29 +0530

    removed error

--- src/App.js
+++ src/App.js
@@ -7,26 +7,26 @@ import Add from "./components/Add";
 import { useState } from "react";
 
 function App() {
-  const [mode, setMode] = useState("light");
+  // const [mode, setMode] = useState("light");
 
-  const darkTheme = createTheme({
-    palette: {
-      mode: mode
-    }
-  });
+  // const darkTheme = createTheme({
+  //   palette: {
+  //     mode: mode
+  //   }
+  // });
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Box bgcolor={"background.default"} color={"text.primary"}>
+    <div>
+      <Box>
         <Navbar />
         <Stack direction="row" spacing={2} justifyContent="space-between">
-          <Sidebar setMode={setMode} mode={mode} />
+          <Sidebar />
           <Feed />
           <Rightbar />
         </Stack>
         <Add />
       </Box>
-    </ThemeProvider>
+    </div>
   );
 }
 
